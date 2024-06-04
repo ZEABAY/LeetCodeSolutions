e86384b8297197834a6982cfa999e7f70b26f972
Zeynel Abiddin Aydar <s-zaaydar@ziraatteknoloji.com>
2024-06-04 13:14:53 +0300

    https://leetcode.com/problems/two-sum/description/

---
+++ app/src/main/java/com/zaaydar/leetcodesolutions/TwoSum.kt
@@ -0,0 +1,26 @@
+package com.zaaydar.leetcodesolutions
+
+
+internal object TwoSum {
+    fun twoSum(nums: IntArray, target: Int): IntArray? {
+        // i solved this in java , ill just convert
+        val hashMap: MutableMap<Int, Int> = HashMap()
+
+        for (i in nums.indices) {
+
+            val search = target - nums[i]
+
+            if (hashMap.containsKey(search)) {
+                // !! is not safe but it will never be null so can be used
+                return intArrayOf(hashMap[search]!!, i)
+            }
+
+            hashMap[nums[i]] = i
+        }
+        // if array contains twoSum of a target this will never work
+        return null
+    }
+}
+// time complexity O(n)
+// memory complexity o(1)
+// 1
