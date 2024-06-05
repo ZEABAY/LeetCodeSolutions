3a2df229a2a6329e7a17439fc26b696d4c7db10a
Zeynel Abiddin Aydar <s-zaaydar@ziraatteknoloji.com>
2024-06-05 10:52:26 +0300

    https://leetcode.com/problems/fizz-buzz/description/

---
+++ app/src/main/java/com/zaaydar/leetcodesolutions/FizzBuzz.kt
@@ -0,0 +1,19 @@
+package com.zaaydar.leetcodesolutions
+
+class FizzBuzz {
+    fun fizzBuzz(n: Int): List<String>? {
+
+        val result: MutableList<String> = mutableListOf()
+        for (i in 1..n) {
+            if (i % 15 == 0) result.add("FizzBuzz")
+            else if (i % 3 == 0) result.add("Fizz")
+            else if (i % 5 == 0) result.add("Buzz")
+            else result.add(i.toString())
+        }
+
+        return result
+    }
+}
+// O(n)
+// O(n)
+// 412
